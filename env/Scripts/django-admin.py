2b933abeaa93573eddc5531a9e3a1e38610b12e6
ferreira <guillaumeferreira00@gmail.com>
2018-06-13 18:59:37 +0200

    environement virtuel et initialisation django

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ferreira\desktop\projet_8_plateforme_nutella\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
